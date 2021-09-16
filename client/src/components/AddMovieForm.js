d642ceeee41c234d6646ec5226dfe21fd81aac16
NeNeTheCreative <shecodes@nenethecreative.com>
2021-09-15 21:31:59 -0400

    added addMovieForm.js

---
+++ client/src/components/AddMovieForm.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from "react"
+import { useParams, useHistory, Link } from "react-router-dom"
+
+import axios from 'axios'
+
+import Movie from "./Movie";
+
+const AddMovieForm = (props) => {
+    const { push } = useHistory();
+    const { id } = useParams();
+    const [newMovie, setNewMovie] = useState({
+        title:"",
+		director: "",
+		genre: "",
+		metascore: 0,
+		description: ""
+    })
+
+
+    const handleChange =(e) => {
+        setNewMovie({
+            ...newMovie,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post(`http://localhost:5000/api/movies/`, newMovie)
+            .then(res => {
+                console.log(res)
+                props.setMovies((movies)=> [...movies, newMovie])
+                push(`/movie/`)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+    const { title, director, genre, metascore, description } = newMovie
+
+    return (
+        <div className="col">
+            <div className="modal-content">
+                <form onSubmit={handleSubmit}>
+                    <div className="modal-header">						
+                        <h4 className="modal-title">Adding Movie</h4>
+                    </div>
+                    <div className="modal-body">					
+                        <div className="form-group">
+                            <label>Title</label>
+                            <input value={title} onChange={handleChange} name="title" type="text" className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <label>Director</label>
+                            <input value={director} onChange={handleChange} name="director" type="text" className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <label>Genre</label>
+                            <input value={genre} onChange={handleChange} name="genre" type="text" className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <label>Metascore</label>
+                            <input value={metascore} onChange={handleChange} name="metascore" type="number" className="form-control"/>
+                        </div>		
+                        <div className="form-group">
+                            <label>Description</label>
+                            <textarea value={description} onChange={handleChange} name="description" className="form-control"></textarea>
+                        </div>
+                                        
+                    </div>
+                    <div className="modal-footer">			    
+                        <input type="submit" className="btn btn-info" value="Submit"/>
+                        <Link to={`/movies/1`}><input type="button" className="btn btn-default" value="Cancel"/></Link>
+                    </div>
+                </form>
+            </div>
+        </div>);
+
+
+}
+
+export default AddMovieForm;
